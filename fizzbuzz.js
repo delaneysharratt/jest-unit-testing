a4a6df0abe6cbf5e6a7c4077c8f8944f1dd6bd94
Laney McKee <laneymckeephotography@gmail.com>
2019-09-23 20:54:06 -0500

    base goal 1 completed

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,1 +1,13 @@
-let fizzBuzz = require('./fizzbuzz.js');
+const fizzBuzz = function(num) {
+  if (num % 15 === 0) {
+    return 'FizzBuzz';
+  } else if (num % 5 === 0) {
+    return 'Buzz';
+  } else if (num % 3 === 0) {
+    return 'Fizz';
+  } else {
+    return num;
+  }
+};
+
+module.exports = fizzBuzz;
--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -1,5 +1,33 @@
-const fizzBuzz = function(num) {
-  // insert if-statements here
-};
+let sum = require('./fizzbuzz.js');
 
-module.exports = fizzBuzz;
+describe('FizzBuzz tests', () => {
+  //if the number is divisible by 3, return "Fizz"
+  test(`Divisible by 3 gives 'Fizz'`, () => {
+    expect(sum(3)).toBe('Fizz');
+    expect(sum('3')).toBe('Fizz');
+    expect(sum(9)).toBe('Fizz');
+    expect(sum(18)).toBe('Fizz');
+  });
+  //if the number is divisible by 5, return "Buzz"
+  test(`Divisible by 5 gives 'Buzz'`, () => {
+    expect(sum(5)).toBe('Buzz');
+    expect(sum('5')).toBe('Buzz');
+    expect(sum(25)).toBe('Buzz');
+    expect(sum(100)).toBe('Buzz');
+  });
+  //if the number is divisible by both 3 and 5, return "FizzBuzz"
+  test(`Divisible by 5 & 3 gives 'FizzBuzz'`, () => {
+    expect(sum(15)).toBe('FizzBuzz');
+    expect(sum('15')).toBe('FizzBuzz');
+    expect(sum(45)).toBe('FizzBuzz');
+    expect(sum(180)).toBe('FizzBuzz');
+  });
+  //otherwise, just return the number that was passed into the function
+  test(`Divisible by neither 5 & 3 gives orig number`, () => {
+    expect(sum(11)).toBe(11);
+    expect(sum('11')).toBe('11');
+    expect(sum(19)).toBe(19);
+    expect(sum(11)).toBe(11);
+    expect(sum('ab')).toBe('ab');
+  });
+});
